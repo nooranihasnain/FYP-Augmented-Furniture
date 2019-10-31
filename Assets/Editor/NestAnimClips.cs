597fcd9b035c0a976b7b60aaf9c06d2dbd3660b5
Zephyr-PC\Zephyr <noorani.hasnain3@gmail.com>
2019-10-31 17:47:03 +0500

    Basic ui layout done

---
+++ Assets/Editor/NestAnimClips.cs
@@ -0,0 +1,59 @@
+﻿//  ==================================================================================================================
+//    <description>NestAnimClips.cs - Nesting AnimationClips inside an AnimationContoller.</description>
+//  <author>ZombieGorilla for Unity Forums</author>
+//     <version>1.0</version>
+//    <date>2016-02-14</date>
+//  ==================================================================================================================
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class NestAnimClips : MonoBehaviour
+{
+    [MenuItem("Assets/Nest AnimClips in Controller")]
+    static public void nestAnimClips()
+    {
+        UnityEditor.Animations.AnimatorController anim_controller = null;
+        List<AnimationClip> clips = new List<AnimationClip>();
+        var objs = Selection.objects;
+
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].GetType() == typeof(UnityEditor.Animations.AnimatorController))
+            {
+                if (anim_controller != null)
+                {
+                    Debug.Log("<color=red>Please Select only ONE controller.</color>");
+                    return;
+                }
+                else
+                {
+                    anim_controller = (UnityEditor.Animations.AnimatorController)objs[i];
+                }
+            }
+
+            if (objs[i].GetType() == typeof(AnimationClip)) { clips.Add((AnimationClip)objs[i]); }
+        }
+
+        if (anim_controller != null && clips.Count > 0)
+        {
+            foreach (AnimationClip ac in clips)
+            {
+                var new_ac = Object.Instantiate(ac) as AnimationClip;
+                new_ac.name = ac.name;
+
+                AssetDatabase.AddObjectToAsset(new_ac, anim_controller);
+                AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(new_ac));
+                AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(ac));
+            }
+            Debug.Log("<color=orange>Added " + clips.Count.ToString() + " clips to controller: </color><color=yellow>" + anim_controller.name + "</color>");
+        }
+        else
+        {
+            Debug.Log("<color=red>Nothing done. Select a controller and anim clips to nest.</color>");
+        }
+    }
+}
+
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public bool IsOpen = false;
+    public Animator WindowAnimator;
+    public GameObject OpenButton;
+    public GameObject CloseButton;
+
+    public GameObject[] FurnitureButtons;
+    private GameObject SelectedChoice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenWindow()
+    {
+        if(!IsOpen)
+        {
+            OpenButton.SetActive(false);
+            CloseButton.SetActive(true);
+            WindowAnimator.Play("Open");
+            IsOpen = true;
+        }
+    }
+
+    public void CloseWindow()
+    {
+        if(IsOpen)
+        {
+            OpenButton.SetActive(true);
+            CloseButton.SetActive(false);
+            WindowAnimator.Play("Close");
+            IsOpen = false;
+        }
+    }
+}
